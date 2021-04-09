d775aec8a683924eaef00fee942d35da566f0829
Roman <rombim999@gmail.com>
2021-04-09 13:16:39 +0200

    check condition of qty of movies

--- js/script.js
+++ js/script.js
@@ -18,8 +18,23 @@ const d = prompt("What´s the score?", ""); */
 for (let i = 0; i < 2; i++) {
     const a = prompt("What is your last movie you´ve seen?", "");
     const b = prompt("What´s the score?", "");
-    personalMovieDB.movies[a] = b;
+    
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
 }
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+    console.log("You seen few movies");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("You are a classic");
+} else if (personalMovieDB.count >= 30) {
+    console.log("You a movie lover");
+} else {
+    console.log("Error");
+}
 
 
